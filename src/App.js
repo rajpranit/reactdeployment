0f960f2d638f1e5e1cf5cff405c9016b5efe9ed0
rajpranit <rajpranit1207@gmail.com>
2022-11-09 14:38:55 +0530

    LazyLoading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
+import React, { Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import NewQuote from "./pages/NewQuote";
-import QuoteDetail from "./pages/QuoteDetail";
-import AllQuotes from "./pages/AllQuotes";
+// import NewQuote from "./pages/NewQuote";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import AllQuotes from "./pages/AllQuotes";
 import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const AllQuotes = React.lazy(() => import("./pages/AllQuotes"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/quote-form">
-            <NewQuote />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div className="centered focused">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="/quote-form">
+              <NewQuote />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { Wrapper } from "./preview";
+
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
@@ -9,9 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     {" "}
-    <Wrapper>
-      {" "}
-      <App />
-    </Wrapper>{" "}
+    <App />{" "}
   </BrowserRouter>
 );
